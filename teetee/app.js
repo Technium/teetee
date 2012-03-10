9f86d44003278124d0a30a4b2f561d281ead3149
steve <steve@t220.com>
2012-03-10 20:02:28 +0000

    + ‘teetee/.js$:-min.js’ + ‘teetee/.sass-cache/1507998fdb6cb13838845c12281b9a6955b1922f/_utilities.scssc’ + ‘teetee/.sass-cache/1e95cc9238bf711af157ea0d9f30bfd17a9840d9/_env.scssc’ + ‘teetee/.sass-cache/1e95cc9238bf711af157ea0d9f30bfd17a9840d9/ie.scssc’ + ‘teetee/.sass-cache/1e95cc9238bf711af157ea0d9f30bfd17a9840d9/newlayout.scssc’ + ‘teetee/.sass-cache/1e95cc9238bf711af157ea0d9f30bfd17a9840d9/print.scssc’ + ‘teetee/.sass-cache/1e95cc9238bf711af157ea0d9f30bfd17a9840d9/screen.scssc’ + ‘teetee/.sass-cache/1e95cc9238bf711af157ea0d9f30bfd17a9840d9/table.scssc’ + ‘teetee/.sass-cache/5f0e1f8719c833d1a2344168013bc000629b013f/_reset.scssc’ + ‘teetee/.sass-cache/5f0e1f8719c833d1a2344168013bc000629b013f/_support.scssc’ + ‘teetee/.sass-cache/7264a3f1abb3ae3898cf1fb563ea38c0ff2208ed/_clearfix.scssc’ + ‘teetee/.sass-cache/7264a3f1abb3ae3898cf1fb563ea38c0ff2208ed/_hacks.scssc’ + ‘teetee/Kybos.js’ + ‘teetee/Makefile’ + ‘teetee/app-min.js’ + ‘teetee/app.js’ + ‘teetee/config.rb’ + ‘teetee/data/league.json’ + ‘teetee/data/results.json’ + ‘teetee/date-en-GB.js’ ...

---
+++ teetee/app.js
@@ -0,0 +1,309 @@
+/*
+ * TODO
+ *
+ * Common template framework for all pages - pass in (db, type, id, subtype)?
+ *  - ALL of the pages are templates? would clean up DOM a bit...
+ *  - access to results, etc, all are easier
+ * Loading animation during template compilation/rending?
+ * Do prev/next for divisions and clubs, don't for teams
+ * Left menu list - always there? always divisions?
+ * Prioritised-column table... javascript needed, I think; check padding/margin size when centered
+ * Decide on context for club display - never any?
+ * Save stats on a timer?
+ *
+ * Split app.js
+ * Compile with yui
+ */
+
+$dev = 1;
+
+app = {
+	DATA_FILES: [ 'league', 'results' ],
+
+	state: {
+		awaitingHashReload: false,
+	},
+
+	start: function () {
+		app.start = new Date;
+		app.random = Kybos();
+		app.templateCache = {};
+		app.dbParts = app.getConfig('dbParts', {});
+		app.stats = app.getConfig('stats', {});
+		app.updateTimes = app.getConfig('updateTimes', {});
+
+		/* Update stuff */
+		app.displayUpdateDate();
+		app.rebuildDatabase(); // Uses cached data
+		if (app.updateRequired) { setTimeout(app.checkForUpdates, 10); }
+
+		/* Location/back-stack stuff */
+		$(window).hashchange(app.hashChange);
+
+		/* Stats stuff */
+		if ("undefined" == typeof app.stats.id) {
+			app.stats.id = app.random.uint32().toString(16) + app.random.uint32().toString(16);
+		}
+		app.updateStat('loaded', 1);
+	},
+
+	checkForUpdates: function () {
+		app.updateStat('updateWanted', 1);
+		if (navigator.onLine === false) {
+			app.updateStat('updateFailOffline', 1);
+			markUpdateState('offline');
+			return;
+		}
+
+		// fetch!
+		app.markUpdateState('checking');
+		app.fetches = {pending: 1, fail: false, changes: 0};
+		setTimeout(function () {
+			app.fetches = {pending: app.DATA_FILES.length, fail: false};
+			for (i=0; i<app.DATA_FILES.length; i++) {
+				app.fetchData(app.DATA_FILES[i]);
+			}
+		}, $dev ? 1000 : 0);
+	},
+
+	updateFinished: function () {
+		app.displayUpdateDate();
+
+		if (app.fetches.fail) {
+			app.updateStat('updateFailed', 1);
+			app.markUpdateState('fail');
+		} else {
+			app.updateStat('updateCompleted', 1);
+			app.markUpdateState('okay');
+			if (app.fetches.changes) {
+				console.log("changed!");
+				app.rebuildDatabase();
+			} else {
+				console.log("not changed :(");
+			}
+		}
+	},
+
+	rebuildDatabase: function () {
+		console.log("rebuild");
+		app.db = {};
+		for (name in app.dbParts) {
+			$.extend(app.db, app.dbParts[name]);
+		}
+		$('section.articles > article.generated').remove();
+		app.state.awaitingHashReload = true;
+		setTimeout(function () {
+			if (app.state.awaitingHashReload) {
+				app.lastHash = null;
+				$(window).hashchange();
+				app.state.awaitingHashReload = false;
+			}
+		}, 10);
+	},
+
+	displayUpdateDate: function () {
+		var q = $.Enumerable.From(app.updateTimes).Select("$.Value");
+		var dateField = $('.update .status .date');
+		var good;
+		if (q.Any()) {
+			var when = new Date(q.Max());
+			dateField.text(when.toString('d'));
+			good = (when > new Date().add(-7).day());
+		} else {
+			dateField.text('n/a');
+			good = false;
+		}
+
+		dateField.removeClass('good bad');
+		dateField.addClass(good ? 'good' : 'bad');
+
+		app.updateRequired = !good;
+		if ($dev) app.updateRequired = true;
+	},
+
+	markUpdateState: function (state) {
+		console.log("update state = "+state, (new Date).getTime() - app.start);
+		$('.update .notice').attr('class', 'notice '+state);
+	},
+
+	fetchData: function (name, force) {
+		console.log("fetching",name);
+		var lastFetchTime = new Date(app.updateTimes[name] || 0);
+		if (force) lastFetchTime = 0;
+		var url = 'data/'+name+'.json';
+		var req = $.ajax(url, {
+			headers: { 'If-Modified-Since': lastFetchTime.toGMTString() },
+			ifModified: true,
+		});
+		req.success(function (data, status) { app.dataFetched(name, data, req, status); });
+		req.error(function (obj, err, msg) { app.dataFetchFailed(name); })
+	},
+
+	dataFetchFailed: function (name) {
+		console.log("fetch failed for",name);
+		app.fetches.pending -= 1;
+		app.fetches.fail = true;
+
+		if (app.fetches.pending == 0) { app.updateFinished(); }
+	},
+
+	dataFetched: function (name, data, req, status) {
+		if (status == "notmodified") {
+			console.log("fetch notmodified for ",name);
+		} else {
+			console.log("fetch done for",name);
+
+			// insert return data into database
+			app.dbParts[name] = data;
+			//~ $.extend(app.db, data);
+			app.setConfig('dbParts', app.dbParts);
+
+			app.fetches.changes = true;
+
+			// record when it arrived
+			var when = req.getResponseHeader('Last-Modified')
+			app.updateTimes[name] = new Date(when).valueOf();
+			app.setConfig('updateTimes', app.updateTimes);
+		}
+
+		app.fetches.pending -= 1;
+		if (app.fetches.pending == 0) { app.updateFinished(); }
+	},
+
+	hashChange: function (evt) {
+		var newHash = window.location.hash || "#main";
+		console.log('hash change', newHash, evt);
+		if (app.lastHash == newHash) { return; }
+
+		var bits = newHash.substring(1).split('/');
+		if (bits.length == 1) {
+			//~ console.log("single part hash", bits);
+
+			var target = $('.articles .'+bits[0]);
+			if (!target.length) {
+				// doesn't exist
+				//~ console.log("no such article",newHash);
+				target = $('.articles .notfound');
+			}
+
+			var visibles = $('.articles > article:visible');
+			if (visibles.length) {
+				//~ console.log("found previously visible article");
+				visibles.fadeOut('fast', function () {
+					target.fadeIn('fast');
+				});
+			} else {
+				//~ console.log("no previous article");
+				target.fadeIn('fast');
+			}
+		} else if (bits.length == 2) {
+			//~ console.log("multi-part hash", bits);
+
+			var type = bits[0];
+			var id = parseInt(bits[1]);
+			if (type in app.db) {
+				var data = $.Enumerable.From(app.db[type]).Where("$.id == "+id).First();
+				target = app.instantiateTemplate(type, id, data);
+
+				// If the last hash was also a division, do fancy transitions
+				if (app.lastHash && app.lastHash.search('#'+type+'/') != -1) {
+					//~ console.log("fancy transition");
+					var lastId = parseInt(app.lastHash.split('/')[1]);
+					var currentArticle = $($('section.articles > article').filter(':visible')[0]);
+					//~ console.log('current',currentArticle);
+					if (1) { // not till it works!
+						app.switchItems(currentArticle, target,
+							(id < lastId)? 'offRight' : 'offLeft',
+							(id < lastId)? 'offLeft' : 'offRight', 500);
+					} else {
+						currentArticle.fadeOut('fast');
+						target.fadeIn('fast');
+					}
+				} else {
+					//~ console.log("no previous matching article type");
+					var visibles = $('.articles > article:visible');
+					if (visibles.length) {
+						//~ console.log("found previously visible article");
+						visibles.fadeOut('fast');
+						target.fadeIn('fast');
+					} else {
+						//~ console.log("no previous article");
+						target.fadeIn('fast');
+					}
+				}
+			}
+		}
+
+		app.lastHash = newHash;
+		evt.preventDefault();
+		evt.stopPropagation();
+	},
+
+	instantiateTemplate: function (type, id, idList) {
+		var ele = $('section.articles article[id="'+type+'/'+id+'"]').first();
+		if (ele.length == 0) {
+			// Compile the template
+			if (!(type in app.templateCache)) {
+				app.templateCache[type] = $('section.templates .'+type).compile(app.templateMapping[type]);;
+			}
+
+			// Instantiate it for this data
+			var data = app.db[type][id];
+			ele = $(app.templateCache[type](data))
+				.addClass(type).addClass('generated')
+				.attr('id', type+'/'+id).appendTo($('section.articles'));
+		}
+		return ele;
+	},
+
+	switchItems: function (outItem, inItem, outCls, inCls, time) {
+		inItem.removeClass(outCls).addClass(inCls).show();
+		setTimeout(function () {
+			inItem.removeClass(outCls+' '+inCls);
+			outItem.addClass(outCls);
+			setTimeout(function() { outItem.hide().removeClass(outCls); }, time);
+		}, 0);
+	},
+
+	updateStat: function (name, addValue, initValue) {
+		if ('undefined' == typeof initValue) { initValue = 0; }
+		if ('undefined' == typeof addValue) { addValue = 1; }
+
+		var result;
+		console.log(name, addValue);
+
+		app.updateConfig('stats', {}, function () {
+			result = app.stats[name] = (app.stats[name] || initValue) + addValue;
+		});
+
+		return result;
+	},
+
+	getConfig: function (name, def) {
+		var value = localStorage.getItem(name);
+		return (null == value) ? def : JSON.parse(value);
+	},
+
+	setConfig: function (name, value) {
+		localStorage.setItem(name, JSON.stringify(value));
+	},
+
+	updateConfig: function (name, initValue, callback) {
+		if ('undefined' == typeof app[name]) {
+			app[name] = app.getConfig(name, initValue);
+		}
+
+		callback();
+
+		app.setConfig(name, app[name]);
+	},
+
+	templateMapping: {
+		division: {
+			'.content_name': 'name',
+		},
+	},
+}
+
+
+$(document).ready(app.start);
---
+++ teetee/node_modules/pure/README.md
@@ -0,0 +1,13 @@
+<h1>PURE Unobtrusive Rendering Engine</h1>
+
+<p>&nbsp;</p>
+
+<div style="background-color:#DDEEFF;border:1px solid #CCCCCC;padding:10px;">
+<big>Please see the <a href="http://beebole.com/pure/documentation/">documentation</a> for more information</big><br />&nbsp;<br />
+<big>If you have a question or an issue post it on: <a href="http://groups.google.com/group/Pure-Unobtrusive-Rendering-Engine">the discussion group</a></big>
+</div>
+
+<p>&nbsp;</p>
+
+&copy; 2011 Michael Cvilic - BeeBole.  All Rights Reserved
+PURE is an Open Source Software. You can use, redistribute  and/or modify it under the terms of the MIT Licence.
