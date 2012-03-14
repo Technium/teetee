096ac9a02d7263578a39d2cd5586a95d2248cf9b
steve <steve@t220.com>
2012-03-14 23:50:16 +0000

    / ‘grabber/grabber.py’

--- grabber/grabber.py
+++ grabber/grabber.py
@@ -10,7 +10,7 @@ class Averages(Parser):
 			(None, MultipleReMatch('<tr>(.*?)</tr>', skip=1),
 				('average', SingleReMatch('<td.*?>(.*?)</td>', float, skip=0, take=1)),
 				('player', SingleReMatch('<td.*?>(.*?)</td>', skip=1, take=1),
-					('playedUp', SingleReMatch('\(Played Up\)', value=True, optional=True),
+					('playedUp', SingleReMatch('\(Played Up\)', value=True, optional=True)),
 				),
 				('team', SingleReMatch('<td.*?>(.*?)</td>', skip=2, take=1)),
 				('played', SingleReMatch('<td.*?>(.*?)</td>', int, skip=3, take=1)),
