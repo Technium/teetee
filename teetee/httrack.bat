a15e9cb21b0b28cf794a89dc3e8c37545d088119
steve <steve@t220.com>
2012-03-27 18:43:16 +0100

    / ‘teetee/httrack.bat’

--- teetee/httrack.bat
+++ teetee/httrack.bat
@@ -1,11 +1,26 @@
-rem
-PORT=8080
-HOST=localhost
-PATH=/
+rem These should be set to match the local test address (i.e. http://localhost:8080/)
+set PORT=8080
+set HOST=localhost
+set PATH=/
 
-OUTPUT=static
+rem This is the name of the output directory, in the same place as this script
+set OUTPUT=static
 
-CMD=httrack
+rem This should be changed to the full path of the httrack.exe
+set CMD=httrack.exe
+
+rem You should have to touch anything else
+
+set URL=http://%HOST%:%PORT%%PATH%
+
+rem Remove any old output
+del /q /y %OUTPUT%
+
+rem Grab a new copy of the whole site
+%CMD% %URL% --get-files --path=%OUTPUT% --depth=20 --sockets=10 --retries=2 --display --index=0
+
+rem maybe --updatehack
 
 
-%CMD http://%HOST:%PORT%PATH --mirror -c8
+echo Done.
+pause
