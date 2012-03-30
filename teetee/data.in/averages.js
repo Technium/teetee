66b2af24015f0b232c826000f2e23cf8002ac9e0
steve <steve@t220.com>
2012-03-30 18:29:58 +0100

    / ‘teetee/data.in/averages.js’

--- teetee/data.in/averages.js
+++ teetee/data.in/averages.js
@@ -1,15 +1,6 @@
 {
 	averages: [
 		{
-			divisionId: 0,
-			players: [
-				{ playerId: 0, played: 30, won: 30 },
-				{ playerId: 1, played: 3, won: 3 },
-				{ playerId: 2, played: 3, won: 3 },
-				{ playerId: 3, played: 21, won: 19 },
-			],
-		},
-		{
 			divisionId: 1,
 			players: [
 				{ playerId: 0, played: 30, won: 30 },
@@ -20,6 +11,15 @@
 		},
 		{
 			divisionId: 2,
+			players: [
+				{ playerId: 0, played: 30, won: 30 },
+				{ playerId: 1, played: 3, won: 3 },
+				{ playerId: 2, played: 3, won: 3 },
+				{ playerId: 3, played: 21, won: 19 },
+			],
+		},
+		{
+			divisionId: 3,
 			players: [
 				{ playerId: 0, played: 30, won: 30 },
 				{ playerId: 1, played: 3, won: 3 },
