8eaa8301fafec721e04c8e0a0d4f0d9e0e4be2f2
Steve Purchase <steve@t220.com>
2012-03-31 01:24:10 +0100

    Added some proper averages for div 0.

--- teetee/data.in/averages.js
+++ teetee/data.in/averages.js
@@ -3,28 +3,23 @@
 		{
 			divisionId: 1,
 			players: [
+				{ playerId: 132, played: 3, won: 3 },
 				{ playerId: 1, played: 30, won: 30 },
-				{ playerId: 2, played: 3, won: 3 },
-				{ playerId: 3, played: 3, won: 3 },
-				{ playerId: 4, played: 21, won: 19 },
+				{ playerId: 116, played: 23, won: 21 },
+				{ playerId: 4, played: 44, won: 39 },
+				{ playerId: 55, played: 52, won: 45 },
+				{ playerId: 136, played: 5, won: 4 },
+				//~ { playerId: , played: , won:  },
 			],
 		},
 		{
 			divisionId: 2,
 			players: [
-				{ playerId: 1, played: 30, won: 30 },
-				{ playerId: 2, played: 3, won: 3 },
-				{ playerId: 3, played: 3, won: 3 },
-				{ playerId: 4, played: 21, won: 19 },
 			],
 		},
 		{
 			divisionId: 3,
 			players: [
-				{ playerId: 1, played: 30, won: 30 },
-				{ playerId: 2, played: 3, won: 3 },
-				{ playerId: 3, played: 3, won: 3 },
-				{ playerId: 4, played: 21, won: 19 },
 			],
 		},
 	]
